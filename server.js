d49586e2ea573710b0f9a9f53507e796f5d73f9e
Brian Elting <belting01@gmail.com>
2015-09-17 21:32:18 -0700

    Serve on process.env.PORT for heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 app.use(express.static('public'));
 routes(app);
 
-var server = app.listen('3000', function() {
+var server = app.listen(process.env.PORT || 3000, function() {
   var address = server.address();
   console.log('App listening at http://%s:%s', address.address, address.port);
 });
